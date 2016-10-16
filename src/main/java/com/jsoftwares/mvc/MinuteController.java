ca47a2f1f6fe4ec70a33adfdf4e7593c48a97cf2
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-10-17 00:48:10 +0300

    Display the add minutes view

--- src/main/java/com/jsoftwares/mvc/MinuteController.java
+++ src/main/java/com/jsoftwares/mvc/MinuteController.java
@@ -1,6 +1,7 @@
 package com.jsoftwares.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,8 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MinuteController {
 
 	@RequestMapping(value="/add")
-	public String add()
+	public String add(Model model)
 	{
+		model.addAttribute("title", "Add minutes exercised");
 		return "minutes/add";
 	}
 }
