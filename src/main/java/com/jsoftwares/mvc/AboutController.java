32bd7229ec4e0df70dd260b847fd51efcd0a8630
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-10-16 23:47:05 +0300

    Annotate with RequestMapping

--- src/main/java/com/jsoftwares/mvc/AboutController.java
+++ src/main/java/com/jsoftwares/mvc/AboutController.java
@@ -1,10 +1,13 @@
 package com.jsoftwares.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value="/about")
 public class AboutController {
 	
+	@RequestMapping(value="/the-team")
 	public String theTeam()
 	{
 		return "the-team";
