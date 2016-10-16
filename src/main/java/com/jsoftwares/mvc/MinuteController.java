8d8969c1f018b7e619c2bb5064dd1c159de69925
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-10-17 00:45:10 +0300

    A method to display add minutes view

--- src/main/java/com/jsoftwares/mvc/MinuteController.java
+++ src/main/java/com/jsoftwares/mvc/MinuteController.java
@@ -1,8 +1,15 @@
 package com.jsoftwares.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value="/minutes")
 public class MinuteController {
 
+	@RequestMapping(value="/add")
+	public String add()
+	{
+		return "minutes/add";
+	}
 }
